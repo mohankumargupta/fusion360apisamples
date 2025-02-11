c056ab3063d1e8fc873b318cfe0cf026c2360fb4
Mohan Gupta <mohangupta@live.com>
2025-02-11 20:58:51 +1100

    unhappy with result, although process works

--- main.py
+++ main.py
@@ -17,7 +17,7 @@ llm = ChatGoogleGenerativeAI(model='gemini-2.0-flash-exp', api_key=SecretStr(api
 
 TASK = """
 
-What I need is python code samples under the section name "Sketchs"(not Design:Sketch).
+What I need is python code samples under the section name "Sketches"(not Design:Sketch).
 
 This is a 2-step process.
 
@@ -31,22 +31,30 @@ Here is where you get them
 - In the main content area of page, 
   Each link under the heading 'Sketches' (not Design:Sketch)
 
-The result for step 1 will be a json object with schema [{sample name, sample url}]
+For step1, need to construct a list of {sample_name, sample_url, sample_code=null} 
+sample_code is null in step1, it will be filled in step2. 
 
 **Second step**
-Now I need you to go the sample urls 1 by 1 in step1 and collect sample code for each. 
-The final result will be a json object with schema [{sample name, sample url, sample code}]
-where [{sample name, sample url}] is what you got from step 1.
+Now I need you to go each sample url in step1 and collect sample code for each, so step2 produces 
+fills in the sample_code entry in step1.
 
 Here is how to get a single code sample.
 
 1. Open a new tab
-2. Go to sample url.
+2. Go to sample_url.
 3. make sure Python tab is selected.
-4. extract sample code
+4. extract sample code and put in sample_code
 5. close tab
 
 Please make sure that you only visit a link once. Do not visit  a link you have already visited.
+
+The final output will be a json object which looks like this:
+{
+ 'step1': <result of step1>
+ 'step2': <result of step2>
+}
+
+
 """
 
 async def run_search():
@@ -61,7 +69,8 @@ async def run_search():
 	history = await agent.run(max_steps=250)
 	h = history.history
 	final_result = history.final_result()
-	
+	history.save_to_file("history.json")
+
 	with open("results.txt", "w", encoding="utf-8") as f:
 		for i in h:
 			content = i.result[-1].extracted_content
@@ -70,7 +79,5 @@ async def run_search():
 	with open('final_results.txt', "w", encoding="utf-8") as f2:
 	  f2.write(final_result)
     
-	#history.save_to_file("history.json")
-
 if __name__ == '__main__':
 	asyncio.run(run_search())
